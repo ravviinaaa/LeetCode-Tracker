7e200b424ffb8a79df08d30635f6871ce0d8c305
K Ravina <ravinaks1025@gmail.com>
2026-07-09 11:25:08 +0530

    Create 1319-UniqueNumberOfOccurrences

---
+++ 1319-UniqueNumberOfOccurrences/1319-UniqueNumberOfOccurrences.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 11:25:09 AM
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        } 
+        Set<Integer> set = new HashSet<>();
+        for (int count : map.values()) {
+            if (set.contains(count)) {   
+                return false;
+            }
+            set.add(count);              
+        }
+        return true;
+    }
+}
