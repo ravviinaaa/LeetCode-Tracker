863f2ca5ba42810e93edcc36132a79ec09b1fa8d
K Ravina <ravinaks1025@gmail.com>
2026-07-09 11:24:47 +0530

    Create 3242-CountElementsWithMaximumFrequency

---
+++ 3242-CountElementsWithMaximumFrequency/3242-CountElementsWithMaximumFrequency.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 11:24:47 AM
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        byte[] freq = new byte[101];
+        byte max = 0, res = 0;
+        for (int n : nums) {
+            byte f = ++freq[n];
+            if (f > max) {
+                max = f; res = f;
+            } else if (f == max) 
+                res += f;
+        }
+        return res;
+    }
+}
