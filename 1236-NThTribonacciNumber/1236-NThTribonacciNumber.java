b201e79fa6a9ad0fd3cc19a26f9521cd3ea2073f
K Ravina <ravinaks1025@gmail.com>
2026-07-09 11:25:14 +0530

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 11:25:14 AM
+class Solution {
+    public int tribonacci(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return helper(n, memo);
+    }
+    
+    private int helper(int n, Map<Integer, Integer> memo) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        
+        int result;
+        if (n == 0) {
+            result = 0;
+        } else if (n == 1 || n == 2) {
+            result = 1;
+        } else {
+            result = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        }
+        
+        memo.put(n, result);
+        return result;
+    }
+}
