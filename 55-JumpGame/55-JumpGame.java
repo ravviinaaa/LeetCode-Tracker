7e67907cfe80c978b31f397eacf1ff0cde88483f
K Ravina <ravinaks1025@gmail.com>
2026-07-09 11:26:45 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 11:26:45 AM
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;        
+    }
+}
