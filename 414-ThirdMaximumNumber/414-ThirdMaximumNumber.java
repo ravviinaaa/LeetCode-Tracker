af13e38e4c54666d3e586688b0044fc4a5c85797
K Ravina <ravinaks1025@gmail.com>
2026-07-09 11:25:46 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 11:25:46 AM
+class Solution {
+    public int thirdMax(int[] nums) {
+        Long fe = Long.MIN_VALUE, se = Long.MIN_VALUE, te = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num == fe || num == se || num == te) continue;
+
+            if (num > fe) {
+                te = se;
+                se = fe;
+                fe = (long) num;
+            } else if (num > se) {
+                te = se;
+                se = (long) num;
+            } else if (num > te) {
+                te = (long) num;
+            }
+        }
+
+        return (te == Long.MIN_VALUE) ? fe.intValue() : te.intValue();
+    }
+}
