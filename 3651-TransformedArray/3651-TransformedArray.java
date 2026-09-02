4e6907ca6ad809938a9717541aac64bf0bc1eebc
K Ravina <ravinaks1025@gmail.com>
2026-09-02 09:12:36 +0530

    Create 3651-TransformedArray

---
+++ 3651-TransformedArray/3651-TransformedArray.java
@@ -0,0 +1,23 @@
+// Last updated: 9/2/2026, 9:12:36 AM
+class Solution {
+    public int[] constructTransformedArray(int[] A) {
+        int n = A.length;
+        int OFFSET = 1 << 7;
+        int MASK = (1 << 8) - 1;
+
+        for (int i = 0; i < n; i++)
+            A[i] += OFFSET;
+
+        for (int i = 0; i < n; i++) {
+            int cur = A[i] - OFFSET;
+            int idx = ((i + cur) % n + n) % n;
+            int val = A[idx] & MASK;
+            A[i] |= val << 8;
+        }
+
+        for (int i = 0; i < n; i++)
+            A[i] = (A[i] >> 8) - OFFSET;
+
+        return A;
+    }
+}
